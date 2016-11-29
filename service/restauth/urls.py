e354558496fa6ed209aefbd928156447cc7f3545
bopo <ibopo@126.com>
2016-11-29 17:27:16 +0800

    - 增加签名接口 - 增加数字转中文大写类 - 增加融云 sdk 获取用户 token 函数

--- service/restauth/urls.py
+++ service/restauth/urls.py
@@ -11,7 +11,7 @@ from .views import (
 
 urlpatterns = (
     # url(r'^password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
-    # url(r'^password/change/$', PasswordChangeView.as_view(), name='rest_password_change'),
+    url(r'^password/change/$', PasswordChangeView.as_view(), name='rest_password_change'),
     # url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
     # url(r'^login/$', LoginView.as_view(), name='rest_login'),
     # url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),

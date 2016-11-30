4c1f73c8069b9f13257adf2d5cc11398fa8c024a
bopo <ibopo@126.com>
2016-11-30 10:05:38 +0800

    增加后台时间选择

--- service/signature/admin.py
+++ service/signature/admin.py
@@ -8,6 +8,10 @@ from .models import Signature
 class SignatureAdmin(admin.ModelAdmin):
     list_display = ('created', 'signs', 'owner')
     list_filter = (('created', DateTimeRangeFilter),)
+    list_display_links = None
+
+    def has_add_permission(self, request):
+        pass
 
     class Media:
         js = ['/admin/jsi18n/']

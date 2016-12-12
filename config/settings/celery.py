f409478990a44b4a418b74d0c4a8242c04b71629
bopo <ibopo@126.com>
2016-12-12 15:38:57 +0800

    python manage.py makemigrations --merge

--- config/settings/celery.py
+++ config/settings/celery.py
@@ -2,24 +2,14 @@
 from __future__ import unicode_literals
 
 try:
-    from .base import INSTALLED_APPS
+    from .base import *
 except ImportError as e:
     raise e
 
-
+DEBUG = False
 INSTALLED_APPS += ("django_celery_results", 'django_celery_beat',)
-# CELERY_RESULT_BACKEND = 'django-db'
-# CELERY_RESULT_BACKEND = 'django-cache'
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
-#: Only add pickle to this list if your broker is secured
-#: from unwanted access (see userguide/security.html)
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TASK_SERIALIZER = 'json'
-
-# CELERY_RESULT_BACKEND = 'django-cache'
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-

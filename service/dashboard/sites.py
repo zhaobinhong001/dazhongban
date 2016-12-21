fcec94092d1073ee0fe554fe2cb463f5e8da58fa
zhaobinhong <zhaobinhong@bankeys.com>
2016-12-21 08:26:56 +0000

    chart data updata

--- service/dashboard/sites.py
+++ service/dashboard/sites.py
@@ -6,7 +6,7 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 from django.contrib.admin.sites import AdminSite
 
-from .views import HomeView, LineView, AutionView
+from .views import HomeView, LineView, AutionView, SetEnView, SignView, EvidView
 
 
 class DashboardSite(AdminSite):
@@ -18,6 +18,9 @@ class DashboardSite(AdminSite):
             url(r'^$', self.admin_view(HomeView.as_view()), name='index'),
             url(r'^data/$', self.admin_view(LineView.as_view()), name='data'),
             url(r'^aution/$', self.admin_view(AutionView.as_view()), name='aution'),
+            url(r'^seten/$', self.admin_view(SetEnView.as_view()), name='seten'),
+            url(r'^sign/$', self.admin_view(SignView.as_view()), name='sign'),
+            url(r'^evid/$', self.admin_view(EvidView.as_view()), name='evid'),
         ]
 
         del urls[0]

ccf71c5678f54d696704f332caf1e8594bf8c189
bopo <ibopo@126.com>
2017-01-06 09:35:54 +0800

    - 更新 dockerfile

--- config/celery.py
+++ config/celery.py
@@ -4,7 +4,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.celery')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
--- config/settings/celery.py
+++ config/settings/celery.py
@@ -11,6 +11,7 @@ INSTALLED_APPS += (
     'django_celery_beat',
 )
 
+DEBUG = False
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'

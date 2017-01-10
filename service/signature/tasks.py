1bc2d8c403729657fe25388a5ce36c2df6b91f61
bopo <ibopo@126.com>
2017-01-10 10:30:41 +0800

    - 更新 证书数据

--- service/signature/tasks.py
+++ service/signature/tasks.py
@@ -5,7 +5,9 @@ import requests
 from celery import shared_task
 from django.conf import settings
 
+from service.kernel.utils.bank_random import bankcard
 from service.signature.models import Identity
+from service.consumer.models import Bankcard
 
 
 @shared_task
@@ -23,5 +25,21 @@ def query_sign(dn, *args, **kwargs):
                 identity.enddate = result.get('endDate')
 
             identity.save()
+
+            # 创建时，生成模拟银行卡号
+            # identity.owner.bankcard_set.filter(bank=u'收付宝').delete()
+            vcard, _ = Bankcard.objects.get_or_create(owner=identity.owner, bank=u'收付宝')
+            vcard.card = bankcard()
+            vcard.type = u'借记卡'
+            vcard.save()
+
+            # 更新用户等级
+            identity.owner.level = identity.level
+            identity.owner.credit = '50'
+            identity.owner.save()
+
+            # 更新真实姓名
+            identity.owner.profile.name = identity.name
+            identity.owner.profile.save()
 
     return result
--- service/signature/views.py
+++ service/signature/views.py
@@ -253,11 +253,7 @@ class IdentityViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
-        # 创建时，生成模拟银行卡号
-        Bankcard.objects.create(owner=request.user, bank=u'收付宝', card=bankcard(), suffix='', type=u'借记卡', flag='')
-        request.user.level = request.data.get('level')
-        request.user.credit = '50'
-        request.user.save()
+
 
         query_sign.delay(dn=data['dn'])
         return Response(serializer.data)

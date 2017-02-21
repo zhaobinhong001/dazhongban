c79778bef8a1baf672a18e08428eb5da4be854a5
bopo <ibopo@126.com>
2017-02-21 11:30:51 +0800

    - bankpay docker conf

--- docker/bankpays/gunicorn.sh
+++ docker/bankpays/gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 /usr/local/bin/python /app/manage.py migrate
-/usr/local/bin/python bankpay.wsgi --worker-class=tornado -w 4 -b 0.0.0.0:5000 --chdir=/app
+/usr/local/bin/gunicorn bankpay.wsgi --worker-class=tornado -w 4 -b 0.0.0.0:4000 --chdir=/app
